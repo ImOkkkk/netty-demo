c8d9e532f7a24c9e974d69465ee090c0512dff21
ImOkkkk <547717253@qq.com>
2023-05-23 14:19:04 +0800

    feat: yaml&注解的方式整合Resilience4j RateLimiter限流

--- netty-push/src/main/java/com/imokkkk/push/controller/PushController.java
+++ netty-push/src/main/java/com/imokkkk/push/controller/PushController.java
@@ -3,13 +3,16 @@ package com.imokkkk.push.controller;
 import com.imokkkk.push.model.Message;
 import com.imokkkk.push.service.PushService;
 
-import java.util.concurrent.TimeUnit;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author liuwy
@@ -23,8 +26,15 @@ public class PushController {
     @Autowired private PushService pushService;
 
     @PostMapping("/push")
-    public void pushToAll(@RequestBody @Validated Message message) throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(1000);
+    //    @Bulkhead(name = "backend-pushToAll")
+    @RateLimiter(name = "backend-pushToAll", fallbackMethod = "fallbackPushToAll")
+    public String pushToAll(@RequestBody @Validated Message message) throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(100);
         pushService.push(message.complete());
+        return "SUCCESS!";
+    }
+
+    public String fallbackPushToAll(Message message, Throwable t) {
+        return "Sorry, the server is busy now.";
     }
 }
